ae49b9340f9eb8797882d0d4b09b7a52e0c2dfa3
nikhlesh-sharma2002 <nikhleshsharma10012002.com>
2025-05-24 13:48:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+var timer = 60;
+var score = 0;
+var hitrn = 0;
+
+function increaseScore(){
+    score += 10;
+    document.querySelector("#scoreval").textContent = score;
+}
+
+function getNewHit(){
+    hitrn = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").textContent = hitrn;
+    
+}
+
+
+function makeBubble(){
+    var clutter = "";
+ 
+for(var i = 1; i<=108; i++){
+    var rn = Math.floor(Math.random()*10)
+    clutter += `<div class="bubble">${rn}</div>`;
+}
+
+document.querySelector("#pbtm").innerHTML = clutter
+}
+
+
+function runTimer(){
+    var timeinterval = setInterval(function(){
+        if (timer > 0) {
+        timer--;
+        document.querySelector("#timerval").textContent = timer; 
+        } else {
+            clearInterval(timeinterval);
+            document.querySelector("#pbtm").innerHTML = 
+                `<div class="game-over">
+                    <h1 class="game-over-text">Game Over</h1>
+                    <button class="restart-btn">Restart</button>
+                </div>`;
+                document.querySelector(".restart-btn").addEventListener("click", restartGame);
+                }
+    },1000);
+}
+
+document.querySelector("#pbtm")
+.addEventListener("click",function(details){
+    var clickednum = Number(details.target.textContent); 
+    if(clickednum === hitrn){
+       increaseScore(); 
+       makeBubble();
+       getNewHit();
+    }
+});
+
+function restartGame() {
+    timer = 60;
+    score = 0;
+    document.querySelector("#scoreval").textContent = score;
+    document.querySelector("#timerval").textContent = timer;
+    
+    makeBubble();
+    getNewHit();
+    runTimer();
+}
+
+getNewHit();
+makeBubble();
+runTimer(); 
